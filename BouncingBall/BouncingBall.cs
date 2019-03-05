557ffe94d6cf4778a715df4fbce41b6bf749481a
evan999 <emeshberge@gmail.com>
2019-03-04 20:39:37 -0500

    Implement Bouncing Ball Solution. Expected result achieved. Compiles and runs successfully. First attempt at solution.

---
+++ BouncingBall/BouncingBall.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncingBall
+{
+    public class BouncingBall
+    {
+        public static int bouncingBall(double h, double bounce, double window)
+        {
+            int seen = 0; // number of times the boy's mother sees the ball
+            double rebound;
+
+            if (h <= 0 || bounce <= 0 || bounce >= 1 || window > h )
+            {
+                // Invalid inputs
+                return -1;
+            }
+
+            if (h > window)
+            {
+                seen++;
+            }
+
+            rebound = h;
+
+            while (rebound > window)
+            {
+                rebound *= bounce;
+                seen++;            
+            }
+
+            return seen;
+        }
+    }
+}
--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -10,6 +10,10 @@ namespace BouncingBall
     {
         static void Main(string[] args)
         {
+            // BouncingBall ball = new BouncingBall();
+            double result = BouncingBall.bouncingBall(3.00, 0.66, 1.50);
+            Console.WriteLine("Mother has seen the ball " + result + " times.");
+            Console.ReadKey();
         }
     }
 }
